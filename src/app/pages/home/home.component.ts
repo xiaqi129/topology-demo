34ebc7433454581eb949d04e272c954360877e4b
yingfen <yingfen@cisco.com>
2018-09-19 13:39:19 +0800

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     templateUrl: './home.html',
     styleUrls: ['./home.style.less']
-}) export class HomeComponent implements OnInit {
+}) export class HomeComponent implements OnInit{
     ngOnInit() {
 
     }
 }
+
