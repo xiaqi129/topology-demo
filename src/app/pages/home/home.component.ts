b2cadc7a69ef72cfa9d3d179fabdc336b40c6581
minghli <minghli@cisco.com>
2018-11-29 13:33:27 +0800

    Change the project style to less

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     templateUrl: './home.html',
-    styleUrls: ['./home.style.scss']
+    styleUrls: ['./home.style.less']
 }) export class HomeComponent implements OnInit {
     ngOnInit() {
     }
--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.less']
 })
 export class WelcomeComponent implements OnInit {
 
