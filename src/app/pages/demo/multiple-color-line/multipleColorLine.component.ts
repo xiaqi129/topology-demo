03edb0313175c5d30c8baaa041b40ceaf7cc30fe
xiaqi <xiaqi@cisco.com>
2019-09-10 14:33:13 +0800

    fix multiple color line

--- src/app/pages/demo/multiple-color-line/multipleColorLine.component.ts
+++ src/app/pages/demo/multiple-color-line/multipleColorLine.component.ts
@@ -15,7 +15,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-1',
                 location: {
-                    x: 417.14176606005026, y: 534.1260748732018
+                    x: 370.6532663300503, y: 517.3613065332017
                 },
                 style: {
                     color: '0X74b9ff',
@@ -26,7 +26,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-2',
                 location: {
-                    x: 518.2734243515075, y: 667.8808487425488
+                    x: 471.78492462150757, y: 651.1160804025487
                 },
                 style: {
                     color: '0X74b9ff',
@@ -37,7 +37,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-3',
                 location: {
-                    x: 629.1920173163318, y: 294.3461753757147
+                    x: 430, y: 292
                 },
                 style: {
                     color: '0X74b9ff',
@@ -48,7 +48,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-4',
                 location: {
-                    x: 720.5367409344217, y: 452.56828592847893
+                    x: 674.0482412044217, y: 435.80351758847894
                 },
                 style: {
                     color: '0X74b9ff',
@@ -59,7 +59,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-5',
                 location: {
-                    x: 782.5206605324116, y: 583.0607482400362
+                    x: 722, y: 582
                 },
                 style: {
                     color: '0X74b9ff',
@@ -70,7 +70,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-6',
                 location: {
-                    x: 922.8000575173364, y: 142.64868793852864
+                    x: 724, y: 176
                 },
                 style: {
                     color: '0X74b9ff',
@@ -81,7 +81,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-7',
                 location: {
-                    x: 1043.5055851555276, y: 406.89592411943335
+                    x: 997.0170854255276, y: 390.13115577943336
                 },
                 style: {
                     color: '0X74b9ff',
@@ -92,7 +92,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-8',
                 location: {
-                    x: 1089.1779469645733, y: 594.4788386922976
+                    x: 1042.6894472345734, y: 577.7140703522975
                 },
                 style: {
                     color: '0X74b9ff',
@@ -103,7 +103,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-9',
                 location: {
-                    x: 1262.0804595273867, y: 258.4607482400361
+                    x: 1215.5919597973866, y: 241.6959799000361
                 },
                 style: {
                     color: '0X74b9ff',
@@ -114,7 +114,7 @@ export class MultipleColorLineComponent implements OnInit {
             {
                 name: 'name-10',
                 location: {
-                    x: 1395.8352333967334, y: 423.20748190837804
+                    x: 1349.3467336667336, y: 406.44271356837805
                 },
                 style: {
                     color: '0X74b9ff',
@@ -510,6 +510,45 @@ export class MultipleColorLineComponent implements OnInit {
                         ratio: 1.85,
                         style: {
                             fill: 0X20c1a1,
+                        }
+                    }
+                }
+            },
+            {
+                name: '11',
+                local_host: 'name-4',
+                remote_host: 'name-7',
+                startLineRatio: 0.25,
+                startLineStyle: {
+                    color: 0X20c1a1,
+                    opacity: 1,
+                },
+                endLineRatio: 0.65,
+                endLineStyle: {
+                    color: 0Xef5050,
+                    opacity: 1,
+                },
+                endArrowStyle: {
+                    color: 0Xef5050,
+                    opacity: 1,
+                },
+                startArrowStyle: {
+                    color: 0X20c1a1,
+                    opacity: 1,
+                },
+                labels: {
+                    1: {
+                        content: '25%',
+                        ratio: 0.1,
+                        style: {
+                            fill: 0X20c1a1,
+                        }
+                    },
+                    2: {
+                        content: '65%',
+                        ratio: 1.35,
+                        style: {
+                            fill: 0Xef5050,
                         }
                     }
                 }
