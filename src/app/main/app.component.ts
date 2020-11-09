df3e2cc0a67ee227565f73b2359eb671953fb220
齐祥 <191433211@qq.com>
2020-11-09 14:08:04 +0800

    添加全局loading

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
+$(window).on('load', function (event) {
+  $('.preloader').delay(500).fadeOut(500);
+});
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+
 export class AppComponent {
   public title = 'network-demo';
   constructor() {
+
   }
 }
